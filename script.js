30e8f25f8bd0678dbeae5ebddedc4c6280fc9524
iga waclawska <iga.waclawska@gmail.com>
2024-10-30 18:58:03 +0100

    give feedback on the Assignment-1

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-let counter = 0;  
+let counter = 0;
 
-        function updateCounter() {
-            document.getElementById('counterValue').textContent = counter;
-        }
+function updateCounter() {
+  // It's best to retrieve the (counterValue) element from the DOM and save it as a const variable at the top of the script.
+  // This prevents repeated DOM access every time the function runs, improving performance.
+  document.getElementById("counterValue").textContent = counter;
+}
 
-        function increase() {
-            counter++;
-            updateCounter();
-        }
+function increase() {
+  counter++;
+  updateCounter();
+}
 
-        function decrease() {
-            counter--;
-            updateCounter();
-        }
+function decrease() {
+  counter--;
+  updateCounter();
+}
 
-        function resetCounter() {
-            counter = 0;
-            updateCounter();
-        }
-        
+function resetCounter() {
+  counter = 0;
+  updateCounter();
+}
